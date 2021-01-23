66c95bd9d5ce1202c8efd158e4d39157a2d6ae2a
saikeerthi555 <43855677+saikeerthi555@users.noreply.github.com>
2021-01-23 15:12:53 +0530

    imported the emojipedia.js in App.jsx before importing the emojipedia we must export the default emojipedia and import it where ever its required can verify by using console.log(emojipedia);

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Entry from "./Entry.jsx";
+import emojipedia from "../emojipedia.js";
 
 //1.Create Entry Component to replace the hardcoded the data
 //2.Create Props to replace hardcoded data
 //3a. import the emojipedia const.
+console.log(emojipedia);
 //3b. map through the emojipedia array and render Entry components
 
 function App() {
